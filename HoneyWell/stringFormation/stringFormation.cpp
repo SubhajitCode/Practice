177adfc0bd39ddd5c634c9ab576da61137289e31
SubhajitCode <subhajitbarh93@gmail.com>
2019-10-27 22:22:39 +0530

    stringFormation Corrected

--- HoneyWell/stringFormation/stringFormation.cpp
+++ HoneyWell/stringFormation/stringFormation.cpp
@@ -1,72 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-int util(vector<string> dict,string target,int i,int j)//i=index of dict  j=index of target
+int util(vector<string> dict,string target,int index,int tindex,int match) //match standsfor no of charecter  matched with taget string
 {
+    
     int m=dict[0].size();
     int n=target.size();
-    if(i>=m||j>=n)
-        return 0;
-    if(j==n-1)
+    if(match==n)
     {
-        int ans=0;
+        return 1;
+    }
+    if(index>=m||tindex>=n)
+        return 0;
+    int ans=0;
+    for(int i=index;i<m;i++)
+    {
         for(string str:dict)
         {
-            if(str[i]==target[j])
+            if(str[i]==target[tindex])
             {
-                ans=ans+1+util(dict,target,i+1,j);
-                cout<<str<<" "<<i<<" "<<endl;
-            }/*
-            else
-            {
-                ans=ans+util(dict,target,i+1,j);
-            }*/
-            
+                ans+=util(dict,target,i+1,tindex+1,match+1);
+            }
         }
-        return  ans;
-
-    }
-    int ans=0;
-    for(string str:dict)
-    {
-        if(str[i]==target[j])
-        {
-            ans=ans+util(dict,target,i+1,j+1);
-        }
-        else
-        {
-            ans=ans+util(dict,target,i+1,j);
-        }
-        
-
     }
     return ans;
+
     
 }
 int stringFormation(vector<string> dict,string target)
 {
-    /*unordered_set<char> set;
-    for(auto ch:target)
-        set.insert(ch);
-    vector<string> dict2;
-    for(string str:dict)
-    {
-        string newstr="";
-        for(int i=0;i<str.size();i++)
-        {
-            if(set.find(str[i])!=set.end())
-            {
-                newstr+=str[i];
-            }
-            re
-        }
-    }
-    */
-
-    return util(dict,target,0,0);
+    return util(dict,target,0,0,0);
 }
 int main()
 {
-    vector<string> dict={"adc","aec","efg"};
-    string target="ac";
+    vector<string> dict={"valya","lyglb","vldoh"};
+    string target="val";
     cout<<stringFormation(dict,target)<<endl;
 }
